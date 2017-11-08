be2bdf3123ebb98aff4f210a833b3e3c0736f62e
Patrick Sigourney <psigourney@gmail.com>
2017-11-08 09:26:17 -0600

    initial commit, empty file

---
+++ Server.java
@@ -0,0 +1,38 @@
+//Distributed Systems Fall 2017
+//Raft Consensus Algorithm
+//Patrick Sigourney & Howie Benefiel
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.lang.reflect.Type;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.*;
+
+
+public class Server{
+    
+    
+    
+    
+    
+    
+    
+    public static void main(String[] args) throws IOException {
+        //Read input file on startup to learn about self and other servers in cluster
+        // <this server #> <count of servers>
+        // 127.0.0.1:9991
+        // 127.0.0.1:9992
+        // 127.0.0.1:9993
+        // etc....
+        
+        
+        return;
+    }
+    
+}
